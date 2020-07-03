14a886393c3d9876f3ab4e3756f55abc1db7e0e7
soramikuu <soramikuu72@gmail.com>
2020-07-03 17:36:32 +0900

    add files

---
+++ otoiawase/20200703/thanks.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>thanks</title>
+</head>
+
+<body>
+    <p>ありがとうございました!</p>
+</body>
+
+</html>
